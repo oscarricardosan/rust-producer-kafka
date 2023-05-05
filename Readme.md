aeec4f9e066f58e557638ddc8ef5fc78a12bc982
Oscar Sancheze <oscarricardosan@gmail.com>
2023-05-05 12:31:30 -0500

    Codigo para transmitir logs a Grafana loki junto a almacenamiento del mismo en cockroachDB.

--- Readme.md
+++ Readme.md
@@ -1,12 +1,24 @@
 # Introducción
 
-Esta aplicación es solo una prueba de concepto, con el fin de evaluar la confiabilidad de los paquetes disponibles en Rust 
+Esta aplicación es solo una prueba de concepto, con el fin de evaluar la confiabilidad de los paquetes disponibles en Rust
 para el uso del broker de mensajería Kafka.
 
-Luego de múltiples pruebas se termino eligiendo el paquete kafka sobre rdkafka, debiodo a que rdkafka generaba entrega exitosa 
+En este branch adicional de conectarse a kafka, almacenara los datos en una base de datos cockroach y se configurara el docker compose para enviar los logs a
+grafana Loki.
+
+Luego de múltiples pruebas se termino eligiendo el paquete kafka sobre rdkafka, debiodo a que rdkafka generaba entrega exitosa
 incluso con el host inaccesible, sin embargo, debo reconocer que la velocidad de rdkafka era mejor.
 
-Por si acaso, en el archivo main.rs queda el código de rdkafka. 
+Por si acaso, en el archivo main.rs queda el código de rdkafka.
+
+# Logs con Grafana Loki
+
+En este caso usaremos el paquete de rust log4rs para guardar el log en un archvo y a promtail para enviar estos archivosa Grafana Loki.
+
+Para log4rs se configura desde el archivo main dentro del método setup_log.
+
+Para promtail lo debemos configurar como un nuevo servicio y el archivo de configuración esta en .devops/docker/develop/promtail_config.yml.
+La carpeta en la que esta el log de la aplicación se monta como volumen en promtail para que los archivos se comuniquen de forma correcta.
 
 # Docker
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+use std::fs;
 use std::time::Duration;
 
 use kafka::error::Error as KafkaError;
 use kafka::producer::{Producer, Record, RequiredAcks};
 
-fn main() {
-    env_logger::init();
+use log::{debug, error, info, trace, warn, LevelFilter, SetLoggerError};
+use log4rs::{
+    append::{
+        console::{ConsoleAppender, Target},
+        file::FileAppender,
+    },
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+    filter::threshold::ThresholdFilter,
+};
 
-    let broker = "haproxy:9095";
-    let topic = "topic-test";
+
+use native_tls::{Certificate, TlsConnector};
+use postgres::{Client, NoTls};
+use postgres_native_tls::MakeTlsConnector;
+
+fn main() {
+
+    setup_log();
+
+    let broker = "kafka-haproxy:9095";
+    let topic = "compras-ejecutadas";
 
     let data = "hello, kafka 12345";
 
     if let Err(e) = produce_message(data, topic, vec![broker.to_owned()]) {
         println!("Failed producing messages: {:?}", e);
     }
+
 }
 
 fn produce_message<'a, 'b>(
@@ -24,24 +43,88 @@ fn produce_message<'a, 'b>(
 
     println!("About to publish a message at {:?} to: {}", brokers, topic);
 
+    let cert = fs::read("db-certificate-cockroach.crt").unwrap();//es el mismo ca.crt
+    let cert = Certificate::from_pem(&cert).unwrap();
+
+    let connector = TlsConnector::builder()
+        .add_root_certificate(cert)
+        .build()
+        .unwrap();
+
+    let connector = MakeTlsConnector::new(connector);
+    let mut client= Client::connect(
+        "postgresql://savne:password@cockroach-haproxy:26257/defaultdb", connector
+    ).unwrap();
+
     let mut producer = Producer::from_hosts(brokers)
         .with_ack_timeout(Duration::from_secs(1))// ~ give the brokers one second time to ack the message
         .with_required_acks(RequiredAcks::One)// ~ require only one broker to ack the message
         .create()?;
 
+
     loop{
+
         for i in 0..100_000{
+
+            let value= format!("{} {}", data, i);
             producer.send(&Record {
                 topic,
                 partition: -1, //causes the producer to find out one on its own using its underlying partitioner.
                 key: format!("{}", i),
-                value: format!("{} {}", data, i).as_bytes(), //we're sending 'data' as a 'value'. there will be no key
+                value: value.as_bytes(), //we're sending 'data' as a 'value'. there will be no key
             }).unwrap();
 
-            println!("enviado {}", i);
+            info!("enviado {}", value);
+
+            let query_result= client.query_one("
+                INSERT INTO events (type, data)
+                VALUES ('producer', $1)
+                returning id", &[ &value],
+            ).unwrap();
+
         }
     }
-    Ok(())
+}
+
+fn setup_log() {
+
+    let level = log::LevelFilter::Info;
+    let file_path = "./log/producerv2.log";
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        // Especifica como quiero que se guarde en el archivo
+        .encoder(Box::new(PatternEncoder::new("{d} {l} {f} {m}\n")))
+        .build(file_path)
+        .unwrap();
+
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(level)))
+                .build("stderr", Box::new(stderr)),
+        )
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stderr")
+                .build(LevelFilter::Info),
+        )
+        .unwrap();
+
+    // Use this to change log levels at runtime.
+    // This means you can change the default log level to trace
+    // if you are trying to debug an issue and need more logs on then turn it off
+    // once you are done.
+    let _handle = log4rs::init_config(config);
+
 }
 
 /*
